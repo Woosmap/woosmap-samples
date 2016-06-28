ff31f6435a594229b22a3de8863ee9b1b1a485bb
gaelsimon <gsimon@webgeoservices.com>
2016-06-28 11:38:46 +0200

    added a default center for initial MapView of Basic Tiled View jsfiddle

--- jsfiddle-samples/basic-tiled-view/demo.js
+++ jsfiddle-samples/basic-tiled-view/demo.js
@@ -4,7 +4,10 @@ function woosmap_main() {
         var loader = new woosmap.MapsLoader();
         var dataSource = new woosmap.DataSource();
         loader.load(function () {
-            var map = new google.maps.Map(woosmap.$('#my-map')[0]);
+            var map = new google.maps.Map(woosmap.$('#my-map')[0], {
+                center: {lat: 42, lng: 3},
+                zoom: 7
+            });
             var storeLayer = new woosmap.TiledView(map);
         });
 
