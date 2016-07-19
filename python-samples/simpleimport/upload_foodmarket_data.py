7a8c3ec789c39e379b47deb816676b51ccbae968
gaelsimon <gsimon@webgeoservices.com>
2016-07-19 16:49:06 +0200

    Used the Replace REST endpoint instead of delete/post for simple import

--- python-samples/simpleimport/upload_foodmarket_data.py
+++ python-samples/simpleimport/upload_foodmarket_data.py
@@ -13,12 +13,12 @@ class InvalidGeometry(Exception):
 def import_location(locations):
     print('Importing locations (%d) ...' % len(locations))
     response = session.post(
-        'http://{api_server_host}/stores'.format(
+        'http://{api_server_host}/stores/replace'.format(
             api_server_host=api_server_host),
         params={'private_key': private_key},
         json={'stores': locations})
 
-    print('...Import time:', response.elapsed.total_seconds())
+    print('Import time:', response.elapsed.total_seconds())
     if response.status_code >= 400:
         print('Import Failed')
         print(response.text)
@@ -30,16 +30,7 @@ def import_location(locations):
 if __name__ == '__main__':
     with open(endpoint_json, 'rb') as f:
         data = json.loads(f.read())
-        failed = []
         session = requests.Session()
-        response = session.delete(
-            'http://{api_server_host}/stores'.format(
-                api_server_host=api_server_host),
-            params={'private_key': private_key})
-
-        if response.status_code >= 400:
-            print(response.text)
-            exit(-1)
 
         try:
             import_location(data["stores"])
